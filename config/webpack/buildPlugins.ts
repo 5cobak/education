98f4e0dae7cca377fac195ee2a41df98639cc3f8
ismagyl <ins-arslanov@mail.ru>
2023-12-16 05:13:19 +0400

    added internationalization

--- config/webpack/buildPlugins.ts
+++ config/webpack/buildPlugins.ts
@@ -1,7 +1,14 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import  webpack, { WebpackPluginInstance }  from 'webpack';
+import webpack, { WebpackPluginInstance } from 'webpack';
 import { BuildOptions } from './types';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-export function buildPlugins({paths}: BuildOptions): WebpackPluginInstance[] {
-	return [new HtmlWebpackPlugin({template: paths.html}), new webpack.ProgressPlugin(), new MiniCssExtractPlugin({filename: 'css/[name].[contenthash].css'})]
+export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
+  return [
+    new HtmlWebpackPlugin({ template: paths.html }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
 }
--- src/widgets/SideBar/ui/Sidebar.tsx
+++ src/widgets/SideBar/ui/Sidebar.tsx
@@ -3,15 +3,25 @@ import s from './index.scss';
 import { useState } from 'react';
 import classNames from 'classnames';
 import ThemeSwitcher from 'src/features/ThemeSwitcher';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 export const Sidebar: React.FC = () => {
   const [collapsed, setCollapse] = useState(true);
 
+  const { t } = useTranslation('common');
+
+  const toggleLocales = () => {
+    const locale = i18next.language;
+    i18next.changeLanguage(locale === 'ru' ? 'en' : 'ru');
+  };
+
   return (
     <div className={classNames(s.sidebar, collapsed && s.collapsed)}>
       <div className={s.switchers}>
+        <Button onClick={toggleLocales}>{t('language')}</Button>
         <ThemeSwitcher />
-        <Button onClick={() => setCollapse(!collapsed)}>{collapsed ? 'show' : 'close'}</Button>
+        <Button onClick={() => setCollapse(!collapsed)}>{collapsed ? t('sidebar_show') : t('sidebar_close')}</Button>
       </div>
     </div>
   );
