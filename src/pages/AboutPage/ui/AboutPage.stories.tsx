5daefc361edf57851fda020858da6b7021a6f92a
arslanov <arslanov@passware.com>
2025-06-10 22:48:40 +0400

    make another stories for pages

---
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta, Story } from '@storybook/react';
+
+import { ThemeDecorator } from 'src/shared/config/storybook/ThemeDecorator';
+
+import { AboutPage as AboutPageComponent } from '..';
+import { Theme } from 'src/shared/lib/theme/ThemeContext';
+
+export default {
+  title: 'pages/MainPage',
+  component: AboutPageComponent,
+} as ComponentMeta<typeof AboutPageComponent>;
+
+const Template: ComponentStory<typeof AboutPageComponent> = (args) => <AboutPageComponent {...args} />;
+
+export const AboutPage: Story = Template.bind({});
+export const AboutPageDarkTheme: Story = Template.bind({});
+
+AboutPageDarkTheme.decorators = [ThemeDecorator(Theme.Dark)];
