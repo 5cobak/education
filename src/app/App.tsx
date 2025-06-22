f61b96f2c94f33d11e20b911466124a671f3ced2
arslanov <arslanov@passware.com>
2025-06-22 17:14:40 +0400

    fixed global theme styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { RouterProvider } from './providers/RouterProvider';
 import { NavBar } from 'src/widgets/Navbar';
-import { useTheme } from 'src/shared/lib/theme';
+
 import { Sidebar } from 'src/widgets/SideBar';
 import '../../config/i18Next/config';
 import { Suspense } from 'react';
@@ -10,11 +10,10 @@ import { PageLoader } from 'src/widgets/PageLoader';
 import { StoreProvider } from './providers/StoreProvider/ui/StoreProvider';
 
 export default function App() {
-  const { theme } = useTheme();
   return (
     <StoreProvider>
       <Suspense fallback={<PageLoader />}>
-        <div className={`app ${theme}`}>
+        <div className="app">
           <BrowserRouter basename="/">
             <NavBar />
             <div className="container">
--- src/shared/lib/theme/useTheme.ts
+++ src/shared/lib/theme/useTheme.ts
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Theme, ThemeContext } from './ThemeContext';
 export const LOCAL_STORAGE_THEME_KEY = 'LocalStorageThemeKey';
 interface UseThemeResult {
@@ -9,9 +9,14 @@ interface UseThemeResult {
 export const useTheme = (): UseThemeResult => {
   const { theme, setTheme } = useContext(ThemeContext);
 
+  useEffect(() => {
+    document.body.className = theme;
+  }, []);
+
   const toggleTheme = () => {
     const newTheme = theme === Theme.Light ? Theme.Dark : Theme.Light;
     setTheme(newTheme);
+    document.body.className = newTheme;
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
